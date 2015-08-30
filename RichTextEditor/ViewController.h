476f173064281decf226bc575e6f2d41b02ff4dc
winningmobile <yurii.bogdanov@yahoo.com>
2015-08-30 12:58:53 +0300

    MTRichTextEditor
    Provide several functions

---
+++ RichTextEditor/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RichTextEdtor
+//
+//  Created by Aryan Gh on 7/21/13.
+//  Copyright (c) 2013 Aryan Ghassemi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RichTextEditor.h"
+
+@interface ViewController : UIViewController <RichTextEditorDataSource>
+
+@end
